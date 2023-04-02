fa0af6a47f036ad83632d6ddbdabe99bf5ea9c12
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-04-02 20:07:57 +0530

    Create 01. Defeat The Monster 1.cpp

---
+++ 01. Defeat The Monster 1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    long long h,x,y;
+	    cin>>h>>x>>y;
+	    if(x>y) cout<<"1"<<endl;
+	    else cout<<"0"<<endl;
+	}
+}
