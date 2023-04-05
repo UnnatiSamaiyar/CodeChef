8157f611b9900ebdde4b34012151b9e76c7a9e8d
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2023-04-05 17:49:51 +0530

    Update 14. FLOW010.cpp

--- 14. FLOW010.cpp
+++ 14. FLOW010.cpp
@@ -12,15 +12,19 @@ int main() {
 	        case 'B':
 	        case 'b':
 	            cout<<"BattleShip"<<endl;
+	            break;
 	        case 'C':
 	        case 'c':
 	            cout<<"Cruiser"<<endl;
+	            break;
 	        case 'D':
 	        case 'd':
 	            cout<<"Destroyer"<<endl;
+	            break;
 	        case 'F':
 	        case 'f':
 	            cout<<"Frigate"<<endl;
+	            break;
 	    }
 	}
 	return 0;
